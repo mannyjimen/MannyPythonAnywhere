0a93f1aef7c18b7624caf043304250aa05db8d8d
mannyjimen <manueljimenez0320@gmail.com>
2026-05-05 23:04:40 -0400

    started updating tables for new project layout, SelectFields look very wonky

--- WardrobeProject/app.py
+++ WardrobeProject/app.py
@@ -4,7 +4,7 @@ from flask_wtf import FlaskForm
 import sqlite3
 import os
 
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import SelectField, StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -20,33 +20,74 @@ def init_db():
     cur.executescript("""
                       PRAGMA foreign_keys = ON;
 
-                      CREATE TABLE IF NOT EXISTS User (
+                      DROP TABLE IF EXISTS Item;
+                      DROP TABLE IF EXISTS Wardrobe;
+                      DROP TABLE IF EXISTS User;
+                      DROP TABLE IF EXISTS Category;
+                      DROP TABLE IF EXISTS Brand;
+                      DROP TABLE IF EXISTS Color;
+
+                      CREATE TABLE User (
                       user_id INTEGER PRIMARY KEY,
                       user_first_name TEXT,
                       user_last_name TEXT
                       );
 
-                      CREATE TABLE IF NOT EXISTS Category (
-                      category_name TEXT
+                      CREATE TABLE Category (
+                      category_name TEXT PRIMARY KEY
                       );
 
-                      CREATE TABLE IF NOT EXISTS Brand (
-                      brand_name TEXT
+                      INSERT INTO Category (category_name)
+                      VALUES
+                      ('Shirt'),
+                      ('Hoodie'),
+                      ('Sweater'),
+                      ('Pants'),
+                      ('Shorts'),
+                      ('Socks'),
+                      ('Other');
+
+                      CREATE TABLE Brand (
+                      brand_name TEXT PRIMARY KEY
                       );
 
-                      CREATE TABLE IF NOT EXISTS Color (
-                      color_name TEXT
+                      INSERT INTO Brand (brand_name)
+                      VALUES
+                      ('Nike'),
+                      ('Adidas'),
+                      ('Reebok'),
+                      ('H&M'),
+                      ('Hollister'),
+                      ('Uniqlo'),
+                      ('Other');
+
+                      CREATE TABLE Color (
+                      color_name TEXT PRIMARY KEY
                       );
 
-                      CREATE TABLE IF NOT EXISTS Item (
+                      INSERT INTO Color (color_name)
+                      VALUES
+                      ('Black'),
+                      ('White'),
+                      ('Grey'),
+                      ('Red'),
+                      ('Blue'),
+                      ('Green'),
+                      ('Brown'),
+                      ('Other');
+
+                      CREATE TABLE Wardrobe (
+                      wardrobe_name VARCHAR PRIMARY KEY
+                      );
+
+                      CREATE TABLE Item (
                       item_id INTEGER PRIMARY KEY,
                       user_id INTEGER REFERENCES User(user_id),
                       item_name TEXT NOT NULL UNIQUE,
+                      wardrobe_name TEXT REFERENCES Wardrobe(wardrobe_name),
                       category_name TEXT REFERENCES Category(category_name),
                       brand_name TEXT REFERENCES Brand(brand_name),
-                      color_name TEXT REFERENCES Color(color_name),
-                      item_is_special BOOLEAN,
-                      item_is_dirty BOOLEAN
+                      color_name TEXT REFERENCES Color(color_name)
                       );
                       """)
 
@@ -55,12 +96,40 @@ init_db()
 class ItemForm(FlaskForm):
     user_id = 0
     item_name = StringField("Item Name", validators=[DataRequired()])
-    category_name = StringField("Category Name")
-    brand_name = StringField("Brand Name")
-    color_name = StringField("Item Color")
-    item_is_special = BooleanField("Is Item Special?")
-    item_is_dirty = BooleanField("Is Item Dirty?")
+
+    #this select field neeeds a query
+    wardrobe_name = StringField("Select Wardrobe")
+    
+    #these can get away without query since constant list always
+    category_name = SelectField("Select Category", choices=[('Shirt', 'Shirt'), \
+                                                            ('Hoodie', 'Hoodie'), \
+                                                            ('Sweater', 'Sweater'), \
+                                                            ('Pants', 'Pants'), \
+                                                            ('Shorts', 'Shorts'), \
+                                                            ('Socks', 'Socks'), \
+                                                            ('Other', 'Other')])
+    
+    brand_name = SelectField("Select Brand", choices=[('Nike', 'Nike'), \
+                                                            ('Adidas', 'Adidas'), \
+                                                            ('Reebok', 'Reebok'), \
+                                                            ('H&M', 'H&M'), \
+                                                            ('Hollister', 'Hollister'), \
+                                                            ('Uniqlo', 'Uniqlo'), \
+                                                            ('Other', 'Other')])
+    
+    color_name = SelectField("Select Color", choices=[('Black', 'Black'), \
+                                                            ('White', 'White'), \
+                                                            ('Grey', 'Grey'), \
+                                                            ('Red', 'Red'), \
+                                                            ('Blue', 'Blue'), \
+                                                            ('Green', 'Green'), \
+                                                            ('Brown', 'Brown'), \
+                                                            ('Other', 'Other')])
+
     submit = SubmitField("Submit")
+
+class WardrobeForm(FlaskForm):
+    wardrobe_name = StringField("Enter name for your new Wardrobe!")
 
 @app.route("/")
 def home():
@@ -73,9 +142,6 @@ def addItem():
     # if form.validate_on_submit():
     #     return redirect(url_for("/"))
     return render_template("form.html", form=form)
-
-# @app.route("/allClothing")
-# def 
 
 if __name__ == "__main__":
     app.run(use_debugger=True)
